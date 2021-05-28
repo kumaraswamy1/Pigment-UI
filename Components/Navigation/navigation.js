66a7ed57688490001e487145169e64eae0f75a54
Kumaraswamy A <41417348+kumaraswamy1@users.noreply.github.com>
2021-05-29 02:35:27 +0530

    docs: Adding Component.js file

--- Components/Navigation/navigation.js
+++
@@ -1,7 +0,0 @@
-const navToggle = document.querySelector(".nav-toggle");
-
-const navClose = document.querySelector(".nav-hide");
-
-navToggle.addEventListener("click", function () {
-	navClose.classList.toggle("nav-mobile");
-});
